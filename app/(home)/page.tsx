84be2194af2885431d4d1c61b82d6bc6c2e87dd1
jmininel <ju.mininel@hotmail.com>
2024-01-31 17:39:08 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,19 @@
+import { format } from "date-fns"
+import { ptBR } from "date-fns/locale";
 import Header from "../_components/header";
 
 export default function Home() {
   return (
-    <main >
-      
-     <Header/>
-    
+    <main>
+      <Header />
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Benjamin!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
     </main>
   );
 }
