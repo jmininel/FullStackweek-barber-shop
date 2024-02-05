aec1f1fdc88b4649e99d82a2d1639ec5f1949239
jmininel <ju.mininel@hotmail.com>
2024-02-05 14:39:28 -0300

    feat: add auth provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "./_components/footer";
+import AuthPRovider from "./_providers/auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} dark`}>
-        {children}
-       <Footer/>
+        <AuthPRovider>
+          {children}
+          <Footer />
+        </AuthPRovider>
       </body>
     </html>
   );
